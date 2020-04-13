788b775a58c4e0f561408a1e80f97164545f0a98
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2020-04-13 17:51:33 -0300

    [DEV] Atualizado SCRIPTS do bin.

--- bin/build.sh
+++ bin/build.sh
@@ -2,3 +2,5 @@
 
 # TODO 
 # Implementar comandos para facilicar o build dos containners em ambiente de produção
+
+# sh bin/build.sh send aws --user=teste --password=teste
--- bin/exec.sh
+++ bin/exec.sh
@@ -2,3 +2,5 @@
 
 # TODO 
 # Implementar comandos para facilicar o uso do docker exec e serviços dos containners
+
+# sh bin/exec.sh bash docker_php php -V
